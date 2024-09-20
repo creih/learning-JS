0efac8a2d3c817ff96aae7b332cb13bfb92f61b9
creih <iherr.9@gmail.com>
2024-09-20 20:48:21 +0200

    started a new dir for learning from javatpoint

---
+++ from_javatpoint/study/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+    return(
+      <div>
+        <h1>"Hello Rwanda!!"</h1>
+        <h1>"Hello Kgl!!"</h1>
+        <h1>"Hello School!!"</h1>
+      </div>
+    );  
+}
+
+export default App;
