e34fea4d9e524ac006d6320ee3b86a5312960081
creih <iherr.9@gmail.com>
2024-09-21 23:00:04 +0200

    used class and extended from Component

--- from_javatpoint/study/src/App.js
+++ from_javatpoint/study/src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React,{Component} from 'react';
 
-function App() {
+class App extends Component {
+  render(){
     return(
       <div>
         <h1>"Hello Rwanda!!"</h1>
         <h1>"Hello Kgl!!"</h1>
         <h1>"Hello School!!"</h1>
       </div>
-    );  
+    );
+  }
 }
 
 export default App;
